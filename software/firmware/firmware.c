083bd6e4921fd3d3c9ad0cc3ba57dd5591b2b52b
laisa_crv <laisa0420@gmail.com>
2020-11-17 21:41:42 +0000

    maximum of 100 generated numbers

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -1,29 +1,24 @@
-//#include "stdlib.h"
+#include "stdlib.h"
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
-#include "iob_timer.h"
+#include "time.h"
 
 int main()
 {
+  srand(time(NULL));
+  uart_init(UART_BASE,FREQ/BAUD);  
+  int i=0;
+  int n=0;
+  int max=0;
   
-  unsigned long long elapsed;
-  unsigned int elapsedu;
-
-  timer_reset(TIMER_BASE);
-  //read current timer count, compute elapsed time
-  //elapsed=timer_get_count(TIMER_BASE);
-  //elapsedu=timer_time_us(TIMER_BASE);
-
-  //init uart 
-  uart_init(UART_BASE,FREQ/BAUD);   
-  uart_printf("\n\n\nHello world!\n\n\n");
-  uart_txwait();
-  elapsed=timer_get_count(TIMER_BASE);
-  elapsedu=timer_time_us(TIMER_BASE);
-  uart_printf("\nExecution time: %d clocks in %dus @%dMHz\n\n", (unsigned int)elapsed, elapsedu, FREQ/1000000);
-  uart_txwait();
+  for(i=0;i<100;i++){
+  	n=rand();
+  	if(n>max) 
+  	  max=n; 
+  	uart_printf("%d\n",n);		
+  } 
+  uart_printf("Maximum number generated was %d\n",max);
   return 0;
-  //char *a = malloc(10);
-  //free(a);
 }
+
